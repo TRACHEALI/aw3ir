415a35fc9de78bca332cd8a47b1c08274f0e3821
TRACHEALI <115471556+TRACHEALI@users.noreply.github.com>
2023-03-03 17:43:39 +0100

    tp6

---
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+ // <--  Ajouter les références ici
+
+import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'meteo/:name',  // la page  affichant la météo prendra comme paramètre 'name'
+    component: MeteoDetailComponent }, // Ce component fera l'appel AJAX et afficher les données reçues par openWeatherMap
+  {
+    path: '', // un chemin vide renverra vers '/'
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: '', // la page principale utilisera le component suivant
+    component: MeteoComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeteoComponent,
+    MeteoDetailComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    // autres imports ici
+    BrowserModule,
+    FormsModule, ReactiveFormsModule //<-- Ajouter les références ici aussi
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
